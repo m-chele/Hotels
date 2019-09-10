dea3b151b6cbeebfcb0b9bba5067cf41d25b2082
m-chele <michele.cenere@gmail.com>
2019-09-10 14:21:47 +0200

    UI hotel list item

--- app/src/main/java/it/m_chele/hotels/MainActivity.java
+++ app/src/main/java/it/m_chele/hotels/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -28,8 +28,10 @@ public class MainActivity extends AppCompatActivity implements HotelsView {
         setSupportActionBar(toolbar);
 
         hotelsListView = findViewById(R.id.hotels_list);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+//        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         hotelsListView.setLayoutManager(layoutManager);
+        hotelsListView.setHasFixedSize(true);
 
         hotelPresenter = new HotelsPresenter(this);
         hotelPresenter.loadData();
