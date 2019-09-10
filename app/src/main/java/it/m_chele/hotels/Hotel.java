95b4844cdc770e771353379b650b2a58fb0a0c20
m-chele <michele.cenere@gmail.com>
2019-09-10 14:51:21 +0200

    UI hotel list item

--- app/src/main/java/it/m_chele/hotels/Hotel.java
+++ app/src/main/java/it/m_chele/hotels/Hotel.java
@@ -1,13 +1,34 @@
 package it.m_chele.hotels;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Hotel {
 
     // TODO: dummy data, prendere da network
 //    @SerializedName("name")
+    public List images;
     public String name;
+    public int stars;
+    public String address;
+    public String checkin;
+    public String checkout;
+    public String email;
+    public String phone;
+    public double rating;
+
 
     public Hotel(String name) {
 
+        this.images = new ArrayList();
+        images.add("https://aff.bstatic.com/images/hotel/max500/102/102202628.jpg");
         this.name = name;
+        this.stars = 4;
+        this.address = "via del Pippo";
+        this.checkin = "17:00 to 22:00";
+        this.checkout = "07:00 to 12:00";
+        this.email = "hotel.pippo@gnail.com";
+        this.phone = "+33 333 777666 ";
+        this.rating = 9.8;
     }
 }
--- app/src/main/java/it/m_chele/hotels/HotelsAdapter.java
+++ app/src/main/java/it/m_chele/hotels/HotelsAdapter.java
@@ -5,9 +5,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.Locale;
 
 public class HotelsAdapter extends RecyclerView.Adapter<HotelsAdapter.HotelViewHolder> {
     private List<Hotel> hotels;
@@ -28,7 +30,15 @@ public class HotelsAdapter extends RecyclerView.Adapter<HotelsAdapter.HotelViewH
     @Override
     public void onBindViewHolder(@NonNull HotelsAdapter.HotelViewHolder hotelViewHolder, int i) {
         Hotel hotel = hotels.get(i);
+//        hotelViewHolder.images.caricaUrls(hotel.images);
         hotelViewHolder.name.setText(hotel.name);
+        hotelViewHolder.stars.setText(String.format("%d stelle", hotel.stars));
+        hotelViewHolder.address.setText(String.format("Indirizzo %s", hotel.address));
+        hotelViewHolder.rating.setText(String.format(Locale.ITALY, "Valutazione utenti %.1f", 9.8));
+        hotelViewHolder.phone.setText(String.format("Tel: %s", hotel.phone));
+        hotelViewHolder.email.setText(String.format("Email: %s", hotel.email));
+        hotelViewHolder.checkin.setText(String.format("Check-in: %s", hotel.checkin));
+        hotelViewHolder.checkout.setText(String.format("Check-in: %s", hotel.checkout));
     }
 
     @Override
@@ -38,12 +48,28 @@ public class HotelsAdapter extends RecyclerView.Adapter<HotelsAdapter.HotelViewH
 
     public class HotelViewHolder extends RecyclerView.ViewHolder {
 
+        public ImageView images;
         public TextView name;
+        public TextView stars;
+        public TextView address;
+        public TextView rating;
+        public TextView phone;
+        public TextView email;
+        public TextView checkin;
+        public TextView checkout;
 
         public HotelViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            images = itemView.findViewById(R.id.hotel_images);
             name = itemView.findViewById(R.id.hotel_name);
+            stars = itemView.findViewById(R.id.hotel_stars);
+            address = itemView.findViewById(R.id.hotel_address);
+            rating = itemView.findViewById(R.id.hotel_rating);
+            phone = itemView.findViewById(R.id.hotel_phone);
+            email = itemView.findViewById(R.id.hotel_email);
+            checkin = itemView.findViewById(R.id.hotel_checkin);
+            checkout = itemView.findViewById(R.id.hotel_checkout);
         }
     }
 }
