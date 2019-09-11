e236a947eddcf81d6c7f36871ecefa72b2a79eb6
m-chele <michele.cenere@gmail.com>
2019-09-11 17:25:28 +0200

    migration to androidx

--- app/src/main/java/it/m_chele/hotels/MainActivity.java
+++ app/src/main/java/it/m_chele/hotels/MainActivity.java
@@ -2,12 +2,12 @@ package it.m_chele.hotels;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
