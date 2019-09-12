7d1788137790e3ec80915ee279b85fee4be9723a
m-chele <michele.cenere@gmail.com>
2019-09-12 16:51:55 +0200

    test for hotelsPresenter

--- app/src/test/java/it/m_chele/hotels/HotelsPresenterTest.java
+++ app/src/test/java/it/m_chele/hotels/HotelsPresenterTest.java
@@ -6,25 +6,25 @@ import org.junit.Test;
 import io.reactivex.Single;
 import it.m_chele.hotels.model.Hotels;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class HotelsPresenterTest {
 
 
+    public static final String EXCEPTION_TEST_MESSAGE = "Exception test message";
     private HotelsPresenter presenter;
     private HotelsViewTest viewStub;
 
     @Before
     public void setUp() {
-        viewStub = new HotelsViewTest();
-        HotelsModel modelStub = () -> succesfullLoad();
-
-        presenter = new HotelsPresenterImpl(viewStub, modelStub);
-
     }
 
     @Test
     public void informs_view_of_loading_status() {
+
+        withLoadingSuccess();
+
         presenter.loadData();
 
         assertTrue(viewStub.showLoadingCalled);
@@ -32,13 +32,34 @@ public class HotelsPresenterTest {
 
     @Test
     public void informs_view_of_refreshing_data_after_loading() {
+        withLoadingSuccess();
+
         presenter.loadData();
 
         assertTrue(viewStub.refreshDataCalled);
     }
 
-    private Single<Hotels> succesfullLoad() {
-        return Single.create(emitter -> emitter.onSuccess(new Hotels()));
+    @Test
+    public void informs_view_of_loading_error() {
+        withLoadingError();
+
+        presenter.loadData();
+
+        assertEquals(EXCEPTION_TEST_MESSAGE, viewStub.showErrorCalledWithMessage);
+    }
+
+    private void withLoadingSuccess() {
+        viewStub = new HotelsViewTest();
+        HotelsModel modelStub = () -> Single.create(emitter -> emitter.onSuccess(new Hotels()));
+
+        presenter = new HotelsPresenterImpl(viewStub, modelStub);
+    }
+
+    private void withLoadingError() {
+        viewStub = new HotelsViewTest();
+        HotelsModel modelStub = () -> Single.create(emitter -> emitter.onError(new Exception(EXCEPTION_TEST_MESSAGE)));
+
+        presenter = new HotelsPresenterImpl(viewStub, modelStub);
     }
 
 
